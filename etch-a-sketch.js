8614eeee364106a3e7ef64999f586c4295bb6ad9
samanthaahannah <samanthaahannah@outlook.com>
2025-05-30 12:35:20 +0100

    Edit generator & add highlighter

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,24 +1,36 @@
 const frame = document.querySelector('div');
 
 rows = document.createElement('div');
-rows.setAttribute("id","row");
 rows.setAttribute("class","row");
 
+pix = document.createElement('div');
+pix.setAttribute("class","pixel");
 
-cols = document.createElement('div');
-cols.setAttribute("class","col");
+/*Grid generator by cloning rows, then cloning pixels within the rows*/
+function generateGrid(){
+    for (let i = 1; i < 17; i++){
+        let clone = pix.cloneNode(true);
+        clone.id = "col"+i;
+        rows.appendChild(clone);
+    }
 
-/*Grid generations*/
-
-for (let i = 0; i < 16; i++){
-    let clone = cols.cloneNode(true);
-    clone.id = "col"+i;
-    rows.appendChild(clone);
+    for (let i = 1; i < 17; ++i){
+        let clone = rows.cloneNode(true);
+        clone.id = "row"+i;
+        frame.appendChild(clone);
+    }
 }
 
-for (let i = 0; i < 16; ++i){
-    let clone = rows.cloneNode(true);
-    clone.id = "row"+i;
-    frame.appendChild(clone);
+/*pixel highlighter*/
+function highlightPixels(){
+    const pixels = document.querySelectorAll('.pixel');
+
+    pixels.forEach((pixel) => {
+        pixel.addEventListener('mouseover',(event) => {
+            pixel.style.backgroundColor = "yellow";
+        })
+    })
 }
 
+generateGrid();
+highlightPixels();
